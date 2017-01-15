b6ef5bbcf081cc4bb5be39177362855377ee130e
Swatantra Mishra <Swatantra Mishra>
2017-01-15 21:21:07 +0530

    added angular and implement UI Router

--- Script/module/app.js
+++ Script/module/app.js
@@ -1,1 +1,18 @@
-﻿var app = angular.module("app", []);
+﻿/// <reference path="../../view/dashboard.html" />
+var app = angular.module("app", ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/Index");
+    //
+    // Now set up the states
+    $stateProvider
+      .state('Index', {
+          url: "/Index",
+          templateUrl: "../../view/dashboard.html",
+          controller: "home.Ctrl"
+      })
+       
+
+});
