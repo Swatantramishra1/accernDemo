acbbeeba6d7e9344714b182a9db46518523d25d3
Swatantra Mishra <Swatantra Mishra>
2017-01-19 02:59:55 +0530

    changes in company page and trying to work on Zendesk

--- Script/controller/home.Ctrl.js
+++ Script/controller/home.Ctrl.js
@@ -1,4 +1,8 @@
-﻿app.controller("home.Ctrl", ['$scope', function ($scope) {
+﻿app.controller("home.Ctrl", ['$scope', '$rootScope', function ($scope, $rootScope) {
 
-
+    $rootScope.Redirect_ToLogin = function ()
+    {
+        window.open('https://app.accern.com/login')
+        
+    }
 }]);
---
+++ plugins/Script/zendesk.js
@@ -0,0 +1,11 @@
+﻿window.zEmbed || (function () {
+    var queue = [];
+
+    window.zEmbed = function () {
+        queue.push(arguments);
+    }
+    window.zE = window.zE || window.zEmbed;
+    document.zendeskHost = 'subdomain.zendesk.com';
+    document.zEQueue = queue;
+}());
+
